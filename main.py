546c730a31f537ff548b7769d51ea1f77586ea9c
Inderight <shinecastle93@gmail.com>
2019-10-23 15:34:28 +0900

    feat: add conditional statements
    Added if statements to show fizz, buzz, fizzbuzz or i.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("python works!")
+i= int(input("type a number(1 to 100) : "))
+if i%15==0:
+    print("fizzbuzz")
+elif i%5==0:
+    print("buzz")
+elif i%3==0:
+    print("fizz")
+
+else:
+    print(i)
+
+
 
